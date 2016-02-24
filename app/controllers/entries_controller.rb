6a0715fc28a8ae7c6e1cf47dbc4613cd409c70cf
weberjavi <weberjavi@gmail.com>
2016-02-24 15:06:54 +0100

    Creados controladores y validaciones para crear nuevas entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -12,4 +12,25 @@ class EntriesController < ApplicationController
     #le otorgaremos valores a los parámetros a través del formulario
     @entry = @project.entries.new
   end
+  def create
+    #find the project
+    @project = Project.find params[:project_id]
+    #new entry for that project with attributes from params[:entry]
+    @entry = @project.entries.new(entry_params)
+    #Try to save it
+    if @entry.save 
+      redirect_to action: "index", controller: "entries", project_it: @project.id
+    else
+      render "new"
+    end
+  end
+
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:hours, :minutes, :date)
+  end
+
+
 end
