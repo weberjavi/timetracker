b347584cb3820ed52490fcea6c7b1c54b8accc34
weberjavi <weberjavi@gmail.com>
2016-02-24 17:55:03 +0100

    Creado link para editar las entradas desde el listado de entradas

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,10 +1,12 @@
 class EntriesController < ApplicationController
+
   def index
-    @project = Project.find_by(id: params[:project_id])
+    @project = Project.find_by id: params[:project_id]
     @entries = @project.entries
     date = Date.today
     @total_hours = @project.total_hours_in_month(date.month, date.year)
   end
+
   def new
     #Identificas el proyecto a través del parámetro definido en la URL
     @project = Project.find params[:project_id]
@@ -12,6 +14,7 @@ class EntriesController < ApplicationController
     #le otorgaremos valores a los parámetros a través del formulario
     @entry = @project.entries.new
   end
+
   def create
     #find the project
     @project = Project.find params[:project_id]
@@ -19,14 +22,36 @@ class EntriesController < ApplicationController
     @entry = @project.entries.new(entry_params)
     #Try to save it
     if @entry.save 
-      redirect_to action: "index", controller: "entries", project_it: @project.id
+      redirect_to action: "index", controller: "entries", project_id: @project.id
     else
       render "new"
     end
   end
 
+  def edit
+    @project = Project.find params[:project_id]
+    @entry = @project.entries.find params[:id]
+  end
+
+  def update
+
+    @project = Project.find params[:project_id]
+    @entry = @project.entries.find params[:id]
+    
+    if @entry.update_attributes(entry_params)
+      redirect_to project_entries_path
+    else
+      render "edit"
+    end
+  end
+
 
   private
+
+  #De esta manera podremos utilizar el método project a la hora de encontrar
+  # def project
+  #   @_project ||= Project.find(params[:project_id])
+  # end
 
   def entry_params
     params.require(:entry).permit(:hours, :minutes, :date)
